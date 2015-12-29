3d66eecfae05ecf7075af2cabd5b4733f6540d1b
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2015-12-29 18:56:06 +0530

    Adds source package URL.

--- Formula/ringo.rb
+++ Formula/ringo.rb
@@ -4,6 +4,8 @@ class Ringo < Formula
   include Language::Haskell::Cabal
   desc "Tool to transform Postgres OLTP database schemas to OLAP database star schemas automatically"
   homepage "https://quintype.github.io/ringo"
+  url "https://github.com/quintype/ringo/archive/v0.1.tar.gz"
+  
   head "https://github.com/quintype/ringo.git"
 
   depends_on "ghc"           => :build
