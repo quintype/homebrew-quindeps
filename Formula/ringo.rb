21305f14616746df4fa8047735519e6cd03360b0
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2015-12-29 19:07:43 +0530

    Moved to latest haskell brew install formula

--- Formula/ringo.rb
+++ Formula/ringo.rb
@@ -2,6 +2,7 @@ require "language/haskell"
 
 class Ringo < Formula
   include Language::Haskell::Cabal
+
   desc "Tool to transform Postgres OLTP database schemas to OLAP database star schemas automatically"
   homepage "https://quintype.github.io/ringo"
   url "https://github.com/quintype/ringo/archive/v0.1.tar.gz"
@@ -12,14 +13,8 @@ class Ringo < Formula
   depends_on "cabal-install" => :build
   depends_on "gmp"
 
-  setup_ghc_compilers
-
   def install
-    cabal_sandbox do
-      cabal_install "--only-dependencies"
-      cabal_install "--prefix=#{prefix}"
-    end
-    cabal_clean_lib
+    install_cabal_package
     (bash_completion/"ringo").write `#{bin}/ringo --bash-completion-script #{bin}/ringo`
   end
 
