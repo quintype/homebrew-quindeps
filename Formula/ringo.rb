464f337f2cef787d15e044acd05c936f0ba55ee9
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2015-12-29 23:01:12 +0530

    Adds brew bottles for ringo

--- Formula/ringo.rb
+++ Formula/ringo.rb
@@ -6,8 +6,14 @@ class Ringo < Formula
   desc "Tool to transform Postgres OLTP database schemas to OLAP database star schemas automatically"
   homepage "https://quintype.github.io/ringo"
   url "https://github.com/quintype/ringo/archive/v0.1.tar.gz"
+  sha256 "ba85b845fc44f8d63e1b508d2747fb132dd04638bad51fedad89e0229de30314"
   
   head "https://github.com/quintype/ringo.git"
+
+  bottle do
+    root_url "https://github.com/quintype/ringo/releases/download/v0.1"
+    sha256 "860b7d12231bf79556a3f525c5982eb208dcfabf384a4a9ba83e8429947f15b0" => :yosemite
+  end
 
   depends_on "ghc"           => :build
   depends_on "cabal-install" => :build
