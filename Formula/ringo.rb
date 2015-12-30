662e49554d1b8462d9df73adfe561d24ba91f628
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2015-12-30 14:30:37 +0530

    Adds a bottle for OSX 10.11

--- Formula/ringo.rb
+++ Formula/ringo.rb
@@ -13,6 +13,7 @@ class Ringo < Formula
   bottle do
     root_url "https://github.com/quintype/ringo/releases/download/v0.1"
     sha256 "860b7d12231bf79556a3f525c5982eb208dcfabf384a4a9ba83e8429947f15b0" => :yosemite
+    sha256 "328f0c6842091bfcddaa7b00dbb3c5bece760053ad5c45550c318954f2b8d06f" => :el_capitan
   end
 
   depends_on "ghc"           => :build
